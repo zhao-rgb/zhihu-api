29273d2f5bea43515de6aea284f204e96b4af8a6
zhao-rgb <1019919091@qq.com>
2020-01-16 22:44:50 +0800

    知乎后端框架初步

--- src/main/java/com/scs/zhihu/api/mapper/SectionMapper.java
+++ src/main/java/com/scs/zhihu/api/mapper/SectionMapper.java
@@ -13,6 +13,11 @@ import java.util.List;
  * @Version 1.0
  **/
 public interface SectionMapper {
+    /**
+     * 根据专题id查询标签列表
+     * @param specialId
+     * @return list<Section>
+     */
     @Select("SELECT * FROM t_section WHERE special_id = #{specialId} ")
     List<Section> getSectionBySpecialId(String specialId);
 }
